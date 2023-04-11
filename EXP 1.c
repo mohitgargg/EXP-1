5984b93e784050adb57fa3bf02d8b16301a03595
mohitgargg <125942814+mohitgargg@users.noreply.github.com>
2023-04-11 12:52:34 +0530

    Add files via upload

---
+++ EXP 1.c
@@ -0,0 +1,15 @@
+#include <windows.h>
+
+#include <stdio.h>
+int main()
+{
+   
+   int var;
+   float flt;
+   char ch;
+   printf("First Value- integer, Second Value- float, Third value- character.");
+    scanf("%d %f %c",&var,&flt,&ch);
+    printf("%d %f %c",var,flt,ch);
+
+   
+}
